8318ae2015702d23593c01eaa75c1bf5895bed84
Polar76 <cobus.opperman@gmail.com>
2020-11-09 19:26:10 +0200

    new local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -120,7 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/CobusOpp/django3-personal-portfolio/static/portfolio'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
-#MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be on production.")
